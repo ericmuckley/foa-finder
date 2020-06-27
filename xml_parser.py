d1267fd43c06cbfaf4048ba785a622c1b94a16d0
Eric Muckley <ericmuckley@gmail.com>
2020-06-27 00:37:41 -0700

    changed keywords

--- xml_parser.py
+++ xml_parser.py
@@ -1,34 +1,30 @@
+# -*- coding: utf-8 -*-
 
 import glob
 import zipfile
-import numpy as np
+import requests
 import pandas as pd
+from datetime import datetime
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 
-from datetime import datetime
 
-import requests
-
-# %%%%%%%%%%%%%%%%%%%% downlaod the xml file %%%%%%%%%%%%%%%%%%%%%%%%%%
-
+# %%%%%%%%%%%%%%%%%%%% download the xml file %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 today = datetime.today().strftime('%Y%m%d')
 today = '20200626'
 url = 'https://www.grants.gov/extract/GrantsDBExtract{}v2.zip'.format(today)
 
-#downloaded_zip = requests.get(url)
 
 
-#url = 'https://www2.census.gov/geo/tiger/GENZ2017/shp/cb_2017_02_tract_500k.zip'
-target_path = 'GrantsDBExtract{}v2.zip'.format(today)
+zip_dir = 'GrantsDBExtract{}v2.zip'.format(today)
 
 response = requests.get(url, stream=True)
 
 
 # if file url is found
 if response.status_code == 200:
-    handle = open(target_path, "wb")
+    handle = open(zip_dir, "wb")
     for chunk in response.iter_content(chunk_size=512):
         if chunk:  # filter out keep-alive new chunks
             handle.write(chunk)
@@ -38,7 +34,6 @@ else:
     print('URL does not exist')
 
 
-#%%
 
 
 # %%%%%%%%%%%%%%%%%%%%%%%%% parse xml file %%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -71,7 +66,7 @@ soup = BeautifulSoup(doc, 'lxml')
 # find all tags present in the xml
 #tags = np.unique([tag.name for tag in soup.find_all()])
 
-titles = [i.text.strip().lower for i in soup.find_all('ns0:opportunitytitle')]
+titles = [i.text.strip().lower() for i in soup.find_all('ns0:opportunitytitle')]
 ids = [i.text.strip() for i in soup.find_all('ns0:opportunityid')]
 #descriptions = [i.text.strip() for i in soup.find_all('ns0:opportuitydescription')]
 
@@ -83,7 +78,7 @@ df_full = pd.DataFrame(columns=['title', 'id'],
 keywords = list(pd.read_csv('keywords.csv').columns)
 keyword_str = '|'.join(keywords).lower()
 
-df = df_full[df_full['title'].str.contains('keyword1|keyword2', na=False)]
+df = df_full[df_full['title'].str.contains(keyword_str, na=False)]
 
 print(df)
 
