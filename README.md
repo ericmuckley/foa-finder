373dc269b487a9e05fbf012ca5b059234fc0b191
Eric Muckley <ericmuckley@gmail.com>
2020-06-28 17:09:16 -0700

    added slack communication

--- README.md
+++ README.md
@@ -1,40 +1,24 @@
 # FOA Finder
 
-This is an automated web scraper for finding funding opportunity announcements from grants.gov. Every day, the [grants.gov](https://www.grants.gov/) database is updated and exported to a zipped XML file which is avilable for download [here](https://www.grants.gov/web/grants/xml-extract.html). This scraper downloads the latest database export, searches it for relevant keywords, and sends matches to a destination.
+This is an automated web scraper for finding funding opportunity announcements from grants.gov. Every day, the [grants.gov](https://www.grants.gov/) database is updated and exported to a zipped XML file which is available for download [here](https://www.grants.gov/web/grants/xml-extract.html). This scraper downloads the latest database export, searches it for relevant keywords, and sends matches to a dedicated Slack channel.
 
 [Information about the format of the XML file](https://www.grants.gov/help/html/help/index.htm?rhcsh=1&callingApp=custom#t=XMLExtract%2FXMLExtract.htm)
-
-`pip install xmltodict`
-
-`pip install beautifulsoup4`
-
-`pip install lxml`
-
-
 
 
 ## Setting environment variables
 
-On macOS: `touch ~/.bash_profile; open ~/.bash_profile`
+Environment variables are required for connecting to the Slack API. TO edit environment variables on macOS: `touch ~/.bash_profile; open ~/.bash_profile`
 
 Add the line `export VAR_NAME="VAR_VALUE"`, where `VAR_NAME` and `VAR_VALUE` are the name and value of the variable.
 
 
 
-
-
-## Testing locally
-
-To test locally: `python app.py`. This will open the Dash page at the localhost: `http://127.0.0.1:8050/`
-
-To stop running the application, use `ctrl + c`
-
-
 ## File description
 
-* **app.py**: Main Python file which creates the application
-* **requirements.txt**: Requirements file for installing app dependencies with pip. This is also used by the application host
-* **Procfile**: file which Heroku uses to launch app
+* **app.py**: Main Python application
+* **keywords.csv**: keywords to use for searching FOA titles
+* **nonkeywords.csv**: keywords to avoid for searching FOA titles
+* **requirements.txt**: Requirements file for installing app dependencies with pip
 
 
 ## Setup for development
@@ -57,17 +41,3 @@ To export the list of installed packages as a `requirements.txt` file: `pip free
 
 To install packages from the requirements file: `pip install -r requirements.txt`
 
-
-
-## Free web hosting on Heroku
-
-Before deploying to the web, make sure that the app is not configured in `debug` mode. This is done by setting the line `app.run_server(debug=False)` in `app.py`.
-
-To host the application on the web using Heroku, first put it in a Github repository. Then follow these steps:
-
-* Setup account on Heroku
-* Create a new app in Heroku
-* Open your new app and go to **Deploy**
-* Choose Github as the deployment method, and connect to your Github repository
-* Scroll to the bottom of the page and click **Deploy now** under the **Manual deploy** section
-* To make changes, edit your files in the Github repo, then re-deploy on Heroku
--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 
+
+"""
+
+What this script does:
+(1) finds the latest FOA database on grants.gov
+(2) downloads the database
+(3) unzips the database to an xml file
+(4) converts the xml database into a pandas dataframe
+(5) filters the FOA dataframe by keywords
+(6) sends the filtered FOA list to a dedicated channel on Slack.
+
+"""
+
+
+import os
 import glob
+import json
 import zipfile
 import requests
 import numpy as np
@@ -9,12 +25,9 @@ from datetime import datetime, timedelta
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 
-import dash
-import dash_html_components as html
 
 
-
-# %%%%%%%%%%%%%%%%%%%% download the xml file %%%%%%%%%%%%%%%%%%%%%%%%%%
+# %%%%%%%%%%%%%%%%%%%% find the database %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 def get_xml_url_and_filename():
@@ -25,10 +38,8 @@ def get_xml_url_and_filename():
     
     file_found = None
     while file_found is None:
-    
         url = 'https://www.grants.gov/extract/GrantsDBExtract{}v2.zip'.format(
             day_to_try.strftime('%Y%m%d'))
-        
         response = requests.get(url, stream=True)
         
         # look back in time if todays data is not posted yet
@@ -36,116 +47,180 @@ def get_xml_url_and_filename():
             file_found = url
         else:
             day_to_try = day_to_try - timedelta(days=1)
-    
+
         filename = 'GrantsDBExtract{}v2.zip'.format(
             day_to_try.strftime('%Y%m%d'))
-    
     return url, filename
-    
 
 
+
+# get url and filename of the latest database available for extraction
 url, filename = get_xml_url_and_filename()
 
 
-# %% 
-
-
-response = requests.get(url, stream=True)
-# if file url is found
-if response.status_code == 200:
-    handle = open(filename, "wb")
-    for chunk in response.iter_content(chunk_size=512):
-        if chunk:  # filter out keep-alive new chunks
-            handle.write(chunk)
-    handle.close()
-# if file url is not found
-else:
-    print('URL does not exist')
 
 
 
-
-# %%%%%%%%%%%%%%%%%%%%%%%%% parse xml file %%%%%%%%%%%%%%%%%%%%%%%%%%
-
-# set name of zip folder
-target_path = 'GrantsDBExtract20200626v2.zip'
+# %%%%%%%%%%%%%%%%%%%% download the database %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-# set name of directory to unzip to
-unzipped_dirname = 'unzipped'
+def download_file_from_url(url, filename):
+    """Download a file from a URL"""
+    response = requests.get(url, stream=True)
+    # if file url is found
+    if response.status_code == 200:
+        handle = open(filename, "wb")
+        for chunk in response.iter_content(chunk_size=512):
+            if chunk:  # filter out keep-alive new chunks
+                handle.write(chunk)
+        handle.close()
+    # if file url is not found
+    else:
+        print('URL does not exist')
 
-# unzip raw file
-with zipfile.ZipFile(filename, 'r') as z:
-    z.extractall(unzipped_dirname)
-
-# get path of file in unzipped folder
-unzipped_filepath = glob.glob(unzipped_dirname+'/*')[0]
 
 
-# parse as tree and convert to string
-tree = ET.parse(unzipped_filepath)
-root = tree.getroot()
-doc = str(ET.tostring(root, encoding='unicode', method='xml'))
+# remove all previously-downloaded zip files
+[os.remove(f) for f in os.listdir() if f.endswith(".zip")]
 
-# initialize beautiful soup object
-soup = BeautifulSoup(doc, 'lxml')   
+# download the database zip file
+download_file_from_url(url, filename)
+
+
+
+
+
+
+# %%%%%%%%%%%%%%%%%%%%%%%%% unzip and parse file %%%%%%%%%%%%%%%%%%%%%
+
+
+def unzip_and_soupify():
+    """Unzip a zip file and parse it using beautiful soup"""
+    # set name of directory to unzip to
+    unzipped_dirname = 'unzipped'
+    
+    # remove all previously-unzipped files
+    [os.remove(f) for f in os.listdir(unzipped_dirname) if f.endswith(".zip")]
+    
+    # unzip raw file
+    with zipfile.ZipFile(filename, 'r') as z:
+        z.extractall(unzipped_dirname)
+    
+    # get path of file in unzipped folder
+    unzipped_filepath = glob.glob(unzipped_dirname+'/*')[0]
+    
+    # parse as tree and convert to string
+    tree = ET.parse(unzipped_filepath)
+    root = tree.getroot()
+    doc = str(ET.tostring(root, encoding='unicode', method='xml'))
+    # initialize beautiful soup object
+    soup = BeautifulSoup(doc, 'lxml')   
+    return soup
+
+
+
+# get beautiful soup object from parsed zip file
+soup = unzip_and_soupify()
+
+
+
+
+
+
 
 # %%%%%%%%%%%%%% convert xml to pandas dataframe %%%%%%%%%%%%%%%%%%%%
 
-# find all tags present in the xml
-#tags = np.unique([tag.name for tag in soup.find_all()])
-
-titles = [i.text.strip().lower() for i in soup.find_all('ns0:opportunitytitle')]
-ids = [i.text.strip() for i in soup.find_all('ns0:opportunityid')]
-#descriptions = [i.text.strip() for i in soup.find_all('ns0:opportuitydescription')]
-
-df_full = pd.DataFrame(columns=['title', 'id'],
-                  data=np.column_stack((titles, ids)))
 
 
-# get keywords to filter dataframe
-keywords = list(pd.read_csv('keywords.csv').columns)
-keyword_str = '|'.join(keywords).lower()
-
-df = df_full[df_full['title'].str.contains(keyword_str, na=False)]
-
-keyword_titles = list(df['title'])
-print(keyword_titles)
-
-
-
-# %%%%%%%%%%%%%%%%%%%% create kanding page for app %%%%%%%%%%%%%%%%%%%%%
-# title for the browser page tab
-TITLE = 'FOA finder'
-app = dash.Dash(__name__)
-app.title = TITLE
-server = app.server  # this line is required for web hosting
-start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+def df_from_soup(soup):
+    """Generate a pandas dataframe from a beautiful-soup xml object"""
+    # find all tags present in the xml
+    #tags = np.unique([tag.name for tag in soup.find_all()])
+    # extract info from title and ID tags
+    titles = [i.text.strip().lower() for i in soup.find_all('ns0:opportunitytitle')]
+    ids = [i.text.strip() for i in soup.find_all('ns0:opportunitynumber')]
+    #descriptions = [i.text.strip() for i in soup.find_all('ns0:opportuitydescription')]
+    df = pd.DataFrame(columns=['title', 'num'],
+                      data=np.column_stack((titles, ids)))
+    return df
 
 
+def filter_df(df):
+    """Filter the dataframe by keywords and nonkeywords (words to avoid).
+    The keywords and nonkeywords are set in external csv files called
+    'keywords.csv' and 'nonkeywords.csv'"""
+    # get keywords to filter dataframe
+    keywords = list(pd.read_csv('keywords.csv', header=None)[0])
+    keywords_str = '|'.join(keywords).lower()
+    # get non-keywords to avoid
+    nonkeywords = list(pd.read_csv('nonkeywords.csv', header=None)[0])
+    nonkeywords_str = '|'.join(nonkeywords).lower()
+    # filter dataframe by keywords and nonkeywords
+    df = df[df['title'].str.contains(keywords_str, na=False)]
+    df = df[~df['title'].str.contains(nonkeywords_str, na=False)]
+    return df
 
-# create html paragraph listing all matching titles
-titles_html = [html.B('Matching titles:'), html.Br()]
-for kt in keyword_titles:
-   titles_html.append(str(kt))
-   titles_html.append(html.Br())
+
+# get full dataframe from database
+df_full = df_from_soup(soup)
+
+# get dataframe filtered by keywords
+df = filter_df(df_full)
 
 
-# create app layout - this is shown at foa-finder.herokuapp.com
-app.layout = html.Div([
+
+
+
+
+
+
+# %%%%%%%%%%%%%%% format string message for Slack %%%%%%%%%%%%%%%%%%%%%%
+
+
+def create_slack_text(print_text=True):
+    """Create text to send into Slack"""
+    # get database date
+    db_date = filename.split('GrantsDBExtract')[1].split('v2')[0]
+    db_date = db_date[:4]+'-'+db_date[4:6]+'-'+db_date[6:]
+    # create text
+    slack_text = 'Funding announcements from grants.gov, extracted {}:'.format(
+        db_date)
+    slack_text += '\n======================================='
+    # loop over each FOA title and add to text
+    for i in range(len(df)):
+        slack_text += '\n{}) {} ({})'.format(
+            i+1, df['title'].iloc[i].upper(), df['num'].iloc[i])
+    slack_text += '\n======================================='
+    slack_text += '\nShowing {} of {} FOAs pulled from grants.gov on {}, using keywords in {}'.format(
+        len(df), len(df_full), db_date, 'https://github.com/ericmuckley/foa-finder/blob/master/keywords.csv.')
+    slack_text += '\nTo view FOA details, go to https://www.grants.gov/web/grants/search-grants.html'
+    slack_text += ' and search by "Opportunity Number". Opportunity numbers are shown in parenthesis after each FOA title in the above list.'
+    if print_text:
+        print(slack_text)
+    return slack_text
+        
     
-    html.H1(children=TITLE),
-    
-    html.P([
-        html.B('App loaded: '), str(start_time), html.Br(),
-        html.B('Newest available database: '), str(filename), html.Br()
-        ]),
-    
-    html.Hr(),
+slack_text = create_slack_text(print_text=True)
 
-    html.P(titles_html)
-    
-    ])
 
-if __name__ == '__main__':
-    app.run_server(debug=False)
+
+
+
+
+# %%%%%%%%%%%%%%%%%%%%% send message in slack %%%%%%%%%%%%%%%%%%%%%%%%
+
+send_to_slack = False
+if send_to_slack:
+    
+    slack_text = 'testing multi-line message: \n line 1 \n line 2'
+
+    try:
+        response = requests.post(
+            os.environ['SLACK_WEBHOOK_URL'],
+            data=json.dumps({'text': slack_text}),
+            headers={'Content-Type': 'application/json'})
+        print('Slack response: ' + str(response.text))
+    except:
+        print('Connection to Slack could not be established.')
+    
+
