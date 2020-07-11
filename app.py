f42e961b8b675d35e7e0bdaa16f4f4ffa723c29d
Eric Muckley <ericmuckley@gmail.com>
2020-07-10 17:22:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ crontab script to run every 24 hours at 18:05 hrs:
 
 
 import os
-import glob
 import json
+import time
 import zipfile
 import requests
 import numpy as np
@@ -60,6 +60,9 @@ def get_xml_url_and_filename():
 
         filename = 'GrantsDBExtract{}v2.zip'.format(
             day_to_try.strftime('%Y%m%d'))
+    
+    print('Found database file {}'.format(filename))
+    
     return url, filename
 
 
@@ -76,6 +79,9 @@ url, filename = get_xml_url_and_filename()
 
 def download_file_from_url(url, filename):
     """Download a file from a URL"""
+    # remove all previously-downloaded zip files
+    [os.remove(f) for f in os.listdir() if f.endswith(".zip")]
+    # ping the dataset url
     response = requests.get(url, stream=True)
     # if file url is found
     if response.status_code == 200:
@@ -84,15 +90,15 @@ def download_file_from_url(url, filename):
             if chunk:  # filter out keep-alive new chunks
                 handle.write(chunk)
         handle.close()
-        print('database successfully downloaded')
+        time.sleep(3)
+        print('Database successfully downloaded')
     # if file url is not found
     else:
         print('URL does not exist')
 
 
 
-# remove all previously-downloaded zip files
-[os.remove(f) for f in os.listdir() if f.endswith(".zip")]
+
 
 # download the database zip file
 download_file_from_url(url, filename)
@@ -100,25 +106,26 @@ download_file_from_url(url, filename)
 
 
 
-
-
 # %%%%%%%%%%%%%%%%%%%%%%%%% unzip and parse file %%%%%%%%%%%%%%%%%%%%%
 
 
-def unzip_and_soupify():
+def unzip_and_soupify(filename, unzipped_dirname='unzipped'):
     """Unzip a zip file and parse it using beautiful soup"""
-    # set name of directory to unzip to
-    unzipped_dirname = 'unzipped'
-    
-    # remove all previously-unzipped files
-    [os.remove(f) for f in os.listdir(unzipped_dirname) if f.endswith(".zip")]
+
+    # remove all previously-downloaded zip files
+    for f in os.listdir(unzipped_dirname):
+        os.remove(os.path.join(unzipped_dirname, f))
     
     # unzip raw file
-    with zipfile.ZipFile(filename, 'r') as z:
+    with zipfile.ZipFile(filename, "r") as z:
         z.extractall(unzipped_dirname)
     
     # get path of file in unzipped folder
-    unzipped_filepath = glob.glob(unzipped_dirname+'/*')[0]
+    unzipped_filepath = os.path.join(
+        unzipped_dirname,
+        os.listdir(unzipped_dirname)[0])
+    
+    print('Unzipping {}'.format(unzipped_filepath))
     
     # parse as tree and convert to string
     tree = ET.parse(unzipped_filepath)
@@ -126,13 +133,14 @@ def unzip_and_soupify():
     doc = str(ET.tostring(root, encoding='unicode', method='xml'))
     # initialize beautiful soup object
     soup = BeautifulSoup(doc, 'lxml')  
-    print('database unzipped')
+    print('Database unzipped')
+    
     return soup
 
 
 
 # get beautiful soup object from parsed zip file
-soup = unzip_and_soupify()
+soup = unzip_and_soupify(filename)
 
 
 
@@ -155,6 +163,9 @@ def df_from_soup(soup):
 
     df = pd.DataFrame(columns=['title', 'num', 'postdate'],
                       data=np.column_stack((titles, ids, postdates)))
+    
+    print('Created dataframe from database')
+    
     return df
 
 
@@ -172,22 +183,17 @@ def filter_df(df):
     
     # filter by post date - the current year and previous year only
     curr_yr = np.max([int(i[-4:]) for i in df['postdate'].values])
-    prev_yr = curr_yr - 1
+    #prev_yr = curr_yr - 1
     df = df[df['postdate'].str.contains('-'+str(curr_yr), na=False)]
     
     # filter dataframe by keywords and nonkeywords
     df = df[df['title'].str.contains(keywords_str, na=False)]
     df = df[~df['title'].str.contains(nonkeywords_str, na=False)]
+    
+    print('Database filtered by keywords')
+    
     return df
 
-
-def preview_tags(soup):
-    """Preview the tags present in a beautiful-soup object"""
-    tags = np.unique([tag.name for tag in soup.find_all()])
-    for t in tags:
-        print(t)
-        print(len(soup(t)))
-        print([i.text.strip().lower() for i in soup.find_all(t)][:5])
 
 
 
@@ -199,7 +205,120 @@ df_full = df_from_soup(soup)
 # get dataframe filtered by keywords
 df = filter_df(df_full)
 
+
+
+
+
+# %%
+
+
+"""
+# example entry from the database:
+
+<OpportunitySynopsisDetail_1_0>
+    <OpportunityID>131073</OpportunityID>
+    <OpportunityTitle>Cooperative Ecosystem Studies Unit, Piedmont South Atlantic Coast CESU</OpportunityTitle>
+    <OpportunityNumber>G12AS20003</OpportunityNumber>
+    <OpportunityCategory>D</OpportunityCategory>
+    <FundingInstrumentType>CA</FundingInstrumentType>
+    <CategoryOfFundingActivity>ST</CategoryOfFundingActivity>
+    <CFDANumbers>15.808</CFDANumbers>
+    <EligibleApplicants>25</EligibleApplicants>
+    <AdditionalInformationOnEligibility>This financial assistance opportunity is being issued under a Cooperative Ecosystem Studies Unit (CESU) Program.  CESUs are partnerships that provide research, technical assistance, and education.  This assistance is provided through a CESU cooperative agreement, which is neither a contract nor a grant.  Eligible recipients must be a participating partner of the Piedmont - South Atlantic Coast CESU Program.</AdditionalInformationOnEligibility>
+    <AgencyCode>DOI-USGS1</AgencyCode>
+    <AgencyName>Geological Survey</AgencyName>
+    <PostDate>11172011</PostDate>
+    <CloseDate>11292011</CloseDate>
+    <LastUpdatedDate>11282011</LastUpdatedDate>
+    <AwardCeiling>0</AwardCeiling>
+    <AwardFloor>0</AwardFloor>
+    <EstimatedTotalProgramFunding>31900</EstimatedTotalProgramFunding>
+    <ExpectedNumberOfAwards>1</ExpectedNumberOfAwards>
+    <Description>The USGS Southeast Ecological Science Center seeks to provide financial assistance for research investigating the use of fish otoliths to identify prime nursery areas for common snook and red drum in Tampa Bay Florida.</Description>
+    <Version>Synopsis 2</Version>
+    <CostSharingOrMatchingRequirement>No</CostSharingOrMatchingRequirement>
+    <ArchiveDate>12172011</ArchiveDate>
+    <AdditionalInformationURL>http://www.grants.gov/</AdditionalInformationURL>
+    <AdditionalInformationText>http://www.grants.gov/ </AdditionalInformationText>
+    <GrantorContactEmail>fgraves@usgs.gov</GrantorContactEmail>
+    <GrantorContactEmailDescription>fgraves@usgs.gov</GrantorContactEmailDescription>
+    <GrantorContactText>Faith Graves, 703-648-7356&amp;lt;br/&amp;gt;fgraves@usgs.gov&amp;lt;br/&amp;gt;</GrantorContactText>
+</OpportunitySynopsisDetail_1_0>
+
+"""
+
+
+
+
+
+
+
+
+def preview_tags(soup):
+    """Preview the tags present in a beautiful-soup object"""
+    tags = np.unique([tag.name for tag in soup.find_all()])
+    
+    """
+    array(['body', 'html', 'ns0:additionalinformationoneligibility',
+       'ns0:additionalinformationtext', 'ns0:additionalinformationurl',
+       'ns0:agencycode', 'ns0:agencyname', 'ns0:archivedate',
+       'ns0:awardceiling', 'ns0:awardfloor', 'ns0:categoryexplanation',
+       'ns0:categoryoffundingactivity', 'ns0:cfdanumbers',
+       'ns0:closedate', 'ns0:closedateexplanation',
+       'ns0:costsharingormatchingrequirement', 'ns0:description',
+       'ns0:eligibleapplicants', 'ns0:estimatedawarddate',
+       'ns0:estimatedprojectstartdate', 'ns0:estimatedsynopsisclosedate',
+       'ns0:estimatedsynopsisclosedateexplanation',
+       'ns0:estimatedsynopsispostdate',
+       'ns0:estimatedtotalprogramfunding', 'ns0:expectednumberofawards',
+       'ns0:fiscalyear', 'ns0:fundinginstrumenttype',
+       'ns0:grantorcontactemail', 'ns0:grantorcontactemaildescription',
+       'ns0:grantorcontactname', 'ns0:grantorcontactphonenumber',
+       'ns0:grantorcontacttext', 'ns0:grants', 'ns0:lastupdateddate',
+       'ns0:opportunitycategory', 'ns0:opportunitycategoryexplanation',
+       'ns0:opportunityforecastdetail_1_0', 'ns0:opportunityid',
+       'ns0:opportunitynumber', 'ns0:opportunitysynopsisdetail_1_0',
+       'ns0:opportunitytitle', 'ns0:postdate', 'ns0:version'],
+      dtype='<U41')
+    """
+    
+    for t in tags:
+        print(t)
+        #if 'ns0:' in t:
+        #    print('{} entries: {}'.format(t.split('ns0:')[1], len(soup(t))))
+            
+    #    print([i.text.strip().lower() for i in soup.find_all(t)][:5])
+
+
+
 #preview_tags(soup)
+
+# %% populate dataframe with every entry tag
+
+
+        
+# list of bs4 objects - each object corresponds to one FOA
+foa_objs = [tag for tag in soup.find_all(
+    ) if 'opportunitysynopsisdetail' in tag.name.lower()]
+
+
+
+dic = {}
+
+
+# loop over each FOA in the database and save its details as a dictionary
+for i, foa in enumerate(foa_objs):
+    dic[i] = {fd.name.split('ns0:')[1]: fd.text for fd in foa.findChildren()}
+
+
+df2 = pd.DataFrame.from_dict(dic, orient='index')
+
+
+# get name and text from each child tag
+#foa_details[0].name
+#foa_details[0].text
+
+
 
 
 # %%%%%%%%%%%%%%% format string message for Slack %%%%%%%%%%%%%%%%%%%%%%
@@ -229,10 +348,14 @@ def create_slack_text(print_text=True):
     slack_text += ' and search by "Opportunity Number". Opportunity numbers are shown in parenthesis after each FOA title in the above list. If the FOA cannot be found, then it has already been closed.'
     if print_text:
         print(slack_text)
+    else:
+        print('Slack text generated')
     return slack_text
 
+
+
     
-slack_text = create_slack_text(print_text=True)
+slack_text = create_slack_text()
 
 
 
